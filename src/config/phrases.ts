aad2e97f8b13fc9e8b97a0ebcd5d9d8c8d51fd78
alexmoderat <rastaboy4@gmx.at>
2025-07-27 14:13:12 +0200

    Update phrases.ts

--- src/config/phrases.ts
+++ src/config/phrases.ts
@@ -1,23 +1,52 @@
+Ist es so korrekt? > root@ubuntu-8gb-nbg1-1: /ha X
+
+GNU nano 7.2
+
 export interface Phrase {
-  word: string;
-  action: number;
-  reason: string;
+
+word: string;
+
+action: number;
+
+reason: string;
+
 }
 
-export const phrases: Phrase[] = [
-  {
-    word: "cheap viewers",
-    action: 0,
-    reason: "triggered banphrase viewbot service",
-  },
-  {
-    word: "separate",
-    action: 1,
-    reason: "please treat with respect",
-  },
-  {
-    word: "kdidfneienixne8nf38r",
-    action: 300,
-    reason: "Toxic behavior 5 minute timeout",
-  },
+export const phrases: Phrase() = [
+
+{
+
+word: "separate",
+
+action: 1,
+
+reason: "please treat with respect"
+
+},
+
+{
+
+phrases.ts
+
+word: "cheap viewers", Best viewers on, smmdex.ru,streamboo .com,NEZHNA.COM, Cheap viewers on, Live Viewers on, Best viewers on
+
+action: 0,
+
+reason: "triggered banphrase viewbot service"
+
+},
+
+{
+
+word: "kdidfneienixne8nf38r",
+
+action: 300,
+
+reason:
+
+"Toxic behavior 5 minute timeout"
+
+},
+
 ];
+
