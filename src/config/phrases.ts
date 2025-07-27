09d5e985bced195bef226f169c0cae99da62fbe0
alexmoderat <rastaboy4@gmx.at>
2025-07-27 13:12:08 +0200

    Update phrases.ts

--- src/config/phrases.ts
+++ src/config/phrases.ts
@@ -1,23 +1,30 @@
 export interface Phrase {
-    word: string;
-    action: number;
-    reason: string;
+  words: string[];
+  action: number;
+  reason: string;
 }
 
 export const phrases: Phrase[] = [
-    {
-        word: "ayri",
-        action: 1,
-        reason: "bitte respektvoll umgehen"
-    },
-    {
-        word: "cheap viewers",
-        action: 0,
-        reason: "triggered banphrase viewbot service"
-    },
-    {
-        word: "kdidfneienixne8nf38r",
-        action: 300,
-        reason: "Toxisches Verhalten - 5 Minuten Timeout"
-    },
+  {
+    words: [
+      "cheap viewers",
+      "Best viewers on",
+      "smmdex.ru",
+      "streamboo.com",
+      "NEZHNA.COM",
+      "Live Viewers on"
+    ],
+    action: 0,
+    reason: "triggered banphrase viewbot service",
+  },
+  {
+    words: ["separate"],
+    action: 1,
+    reason: "please treat with respect",
+  },
+  {
+    words: ["kdidfneienixne8nf38r"],
+    action: 300,
+    reason: "Toxic behavior 5 minute timeout",
+  },
 ];
